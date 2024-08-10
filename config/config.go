ee23e1aef4fb7813c84396ddfcaed1f2ed3e14bb
Abinand P <abinand0911@gmail.com>
2024-08-10 11:43:10 +0530

    Created function to convert the port used in the ip line

--- config/config.go
+++ config/config.go
@@ -148,8 +148,7 @@ func (b *BootstrapDNS) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	return nil
 }
 
-
-func (b *BootStrappedUpstream) UnmarshalYAML (unmarshal func (interface{}) error) error {
+func (b *BootStrappedUpstream) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	if err := unmarshal(&b.Upstream); err == nil {
 		return nil
 	}
@@ -165,7 +164,6 @@ func (b *BootStrappedUpstream) UnmarshalYAML (unmarshal func (interface{}) error
 
 	return nil
 }
-
 
 type Config struct {
 	Upstreams        Upstreams           `yaml:"upstreams"`
@@ -211,12 +209,12 @@ type Config struct {
 type Ports struct {
 	DNS ListenConfig `yaml:"dns" default: "53"`
 
-	HTTP ListenConfig `yaml:"http"` 
-	HTTPS ListenConfig `yaml:"https"` 
-	TLS ListenConfig `yaml:"tls"`
+	HTTP  ListenConfig `yaml:"http"`
+	HTTPS ListenConfig `yaml:"https"`
+	TLS   ListenConfig `yaml:"tls"`
 }
 
-func (c *Ports) LogConfig(logger *logrus.Entry){
+func (c *Ports) LogConfig(logger *logrus.Entry) {
 	logger.Infof("DNS port: %s", c.DNS)
 	logger.Infof("HTTP port: %s", c.HTTP)
 	logger.Infof("HTTPS port: %s", c.HTTPS)
@@ -224,9 +222,8 @@ func (c *Ports) LogConfig(logger *logrus.Entry){
 }
 
 type (
-	BootstrapDNS bootstrapDNS 
+	BootstrapDNS bootstrapDNS
 	bootstrapDNS []BootstrappedUpstream
-
 )
 
 func (b *BootstrapDNS) isEnabled() bool {
@@ -241,20 +238,18 @@ type (
 	BootstrappedUpstream bootstrappedUpstream
 	bootstrappedUpstream struct {
 		Upstream Upstream `yaml:"upstream"`
-		IPs []net.IP `yaml:"ips"`
+		IPs      []net.IP `yaml:"ips"`
 	}
 )
 
 type (
 	FQDNOnly = toEnable
-	EDE = toEnable
+	EDE      = toEnable
 )
-
 
 type toEnable struct {
 	Enable bool `yaml:"enable" default: false`
 }
-
 
 func (e *toEnable) isEnabled() bool {
 	return e.Enable
@@ -272,21 +267,21 @@ func (c *Init) LogConfig(logger *logrus.Entry) {
 	logger.Debugf("Strategy =  %s", c.Strategy)
 }
 
-type SourceLoading struct{
+type SourceLoading struct {
 	Init `yaml:",inline"`
 
-	Concurrency uint `yaml:"concurrency" default:"4"`
-	MaxErrorsPerSource int `yaml:"maxErrorsPerSource" default:"5"`
-	RefreshPeriod Duration `yaml:"refreshPeriod" default:"3h"`
-	Downloads Downloader `yaml:"downloads"`
+	Concurrency        uint       `yaml:"concurrency" default:"4"`
+	MaxErrorsPerSource int        `yaml:"maxErrorsPerSource" default:"5"`
+	RefreshPeriod      Duration   `yaml:"refreshPeriod" default:"3h"`
+	Downloads          Downloader `yaml:"downloads"`
 }
 
-func (c *SourceLoading) LogConfig(logger *logrus.Entry){
+func (c *SourceLoading) LogConfig(logger *logrus.Entry) {
 	c.Init.LogConfig(logger)
 	logger.Infof("Concurrency = %d", c.Concurrency)
 	logger.Debugf("Max errors per source = %d", c.MaxErrorsPerSource)
 
-	if c.RefreshPeriod.IsAboveZero(){
+	if c.RefreshPeriod.IsAboveZero() {
 		logger.Infof("Refresh  = every %s", c.RefreshPeriod)
 	} else {
 		logger.Infof("Refresh = disabled")
@@ -297,7 +292,7 @@ func (c *SourceLoading) LogConfig(logger *logrus.Entry){
 
 }
 
-func (c *SourceLoading) StartPeriodicRefresh(ctx context.Context, refresh func(context.Context) error, logErr func(error),) error{
+func (c *SourceLoading) StartPeriodicRefresh(ctx context.Context, refresh func(context.Context) error, logErr func(error)) error {
 	err := c.Strategy.Do(ctx, refresh, logErr)
 
 	if err != nil {
@@ -307,16 +302,16 @@ func (c *SourceLoading) StartPeriodicRefresh(ctx context.Context, refresh func(c
 	if c.RefreshPeriod > 0 {
 		go c.periodically(ctx, refresh, logErr)
 	}
-	
 
 	return nil
 }
 
-
-func (c *SourceLoading) periodically( ctx context.Context, refresh func(context.Context) error, logErr func(error),){
-	refresh = recoverToError(refresh, func(panicVal any) error{
+func (c *SourceLoading) periodically(
+	ctx context.Context, refresh func(context.Context) error, logErr func(error),
+) {
+	refresh = recoverToError(refresh, func(panicVal any) error {
 		return fmt.Errorf("panic during periodic refresh: %v", panicVal)
-	}
+	})
 
 	ticker := time.NewTicker(c.RefreshPeriod.ToDuration())
 
@@ -324,23 +319,22 @@ func (c *SourceLoading) periodically( ctx context.Context, refresh func(context.
 
 	for {
 		select {
-			case <-ticker.C:
-				err := refresh(ctx)
-				if err != nil {
-					logErr(err)
-				}
-			case <-ctx.Done():
-				return
+		case <-ticker.C:
+			err := refresh(ctx)
+			if err != nil {
+				logErr(err)
+			}
+		case <-ctx.Done():
+			return
 
 		}
 	}
 }
 
-
 func recoverToError(do func(context.Context) error, onPanic func(any) error) func(context.Context) error {
 	return func(ctx context.Context) error {
-		defer func(){
-			if val:= recover(); val != nil {
+		defer func() {
+			if val := recover(); val != nil {
 				rerr = onPanic(val)
 			}
 		}()
@@ -350,22 +344,21 @@ func recoverToError(do func(context.Context) error, onPanic func(any) error) fun
 }
 
 type Downloader struct {
-	Timeout Duration `yaml:"timeout" default:"5s"`
-	Attempts uint `yaml:"attempts" default:"3"`
+	Timeout  Duration `yaml:"timeout" default:"5s"`
+	Attempts uint     `yaml:"attempts" default:"3"`
 	Cooldown Duration `yaml:"cooldown" default:"500ms"`
 }
 
-
-func (d *Downloader) LogConfig(logger *logrus.Entry){
+func (d *Downloader) LogConfig(logger *logrus.Entry) {
 	logger.Infof("Timeout = %s", d.Timeout)
 	logger.Infof("Attempts = %d", d.Attempts)
 	logger.Infof("Cooldown = %s", d.Cooldown)
 }
 
-func WithDefaults[T any]()(T, error){
-	var cfg T 
-	if err:= defaults.Set(&cfg); err!= nil{
-		reurn cfg, fmt.Errorf("can't apply %T defaults: %w", cfg, err)
+func WithDefaults[T any]() (T, error) {
+	var cfg T
+	if err := defaults.Set(&cfg); err != nil {
+		return cfg, fmt.Errorf("can't apply %T defaults: %w", cfg, err)
 	}
 
 	return cfg, nil
@@ -380,9 +373,165 @@ func mustDefault[T any]() T {
 	return cfg
 }
 
-func LoadConfig(path string,mandatory bool)(rCfg *Config, rerr error){
+func LoadConfig(path string, mandatory bool) (rCfg *Config, rerr error) {
 	logger := logrus.NewEntry(log.Log())
 
 	return loadConfig(logger, path, mandatory)
 }
 
+func loadConfig(logger *logrus.Entry, path string, mandatory bool) (rCfg *Config, rerr error) {
+	cfg, err := WithDefaults[Config]()
+
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if rerr == nil {
+			util.LogPrivacy.Store(rCfg.Log.Privacy)
+		}
+	}()
+	fs, err := os.Stat(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return &cfg, nil
+		}
+		return nil, fmt.Errorf("can't read config file: %w", err)
+	}
+
+	var (
+		data       []byte
+		prettyPath string
+	)
+
+	if fs.IsDir() {
+		prettyPath = filepath.Join(path, "*")
+		data, err = readFromDir(path, data)
+		if err != nil {
+			return nil, fmt.Errorf("can't read config files: %w", err)
+		}
+	} else {
+		prettyPath = path
+		data, err = os.ReadFile(path)
+		if err != nil {
+			return nil, fmt.Errorf("can't read config file: %w", err)
+		}
+	}
+
+	cfg.CustomDNS.Zone.configPath = prettyPath
+
+	err = unmarshalConfig(logger, data, &cfg)
+
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+
+}
+
+func readFromDir(path string, data []byte) ([]byte, error) {
+
+	err := filepath.WalkDir(path, func(filePath string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if path == filePath {
+			return nil
+		}
+		if !strings.HasSuffix(filePath, ".yml") && !strings.HasSuffix(filePath, ".yaml") {
+			return nil
+		}
+		isRegular, err := isRegularFile(filePath)
+		if err != nil {
+			return err
+		}
+		if !isRegular {
+			return nil
+		}
+		fileData, err := os.ReadFile(filePath)
+		if err != nil {
+			return err
+		}
+
+		data = append(data, []byte("\n")...)
+		data = append(data, filedata...)
+		return nil
+	})
+	return data, err
+}
+
+func isRegularFile(path string) (bool, error) {
+	stat, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	isRegular := stat.Mode()&os.ModeType == 0
+
+	return isRegular, nil
+}
+
+func unmarshalConfig(logger *logrus.Entry, data []byte, cfg *Config) error {
+	err := yaml.UnmarshalStrict(data, cfg)
+	if err != nil {
+		return fmt.Errorf("Wrong file structure %w", err)
+	}
+	useDepredOpts := cfg.migrate(logger)
+
+	if useDepredOpts {
+		logger.Error("Configuration uses deprecated options, see warning logs for details")
+	}
+
+	cfg.validate(logger)
+	return nil
+}
+
+func (c *Config) migrate(logger *logrus.Entry) bool {
+	usesDepredOpts := Migrate(logger, "", cfg.Deprecated, map[string]Migrator{
+		"upstream":        Move(To("upstreams.groups", &cfg.Upstreams)),
+		"upstreamTimeout": Move(To("upstreams.timeout", &cfg.Upstreams)),
+		"disableIPv6": Apply(To("filtering.queryTypes", &cfg.Filtering), func(oldValue bool) {
+			if oldValue {
+				cfg.Filtering.QueryTypes.Insert(dns.Type(dns.TypeAAAA))
+			}
+		}),
+		"port":         Move(To("ports.dns", &cfg.Ports)),
+		"httpPort":     Move(To("ports.http", &cfg.Ports)),
+		"httpsPort":    Move(To("ports.https", &cfg.Ports)),
+		"tlsPort":      Move(To("ports.tls", &cfg.Ports)),
+		"logLevel":     Move(To("log.level", &cfg.Log)),
+		"logFormat":    Move(To("log.format", &cfg.Log)),
+		"logPrivacy":   Move(To("log.privacy", &cfg.Log)),
+		"logTimestamp": Move(To("log.timestamp", &cfg.Log)),
+		"dohUserAgent": Move(To("upstreams.userAgent", &cfg.Upstreams)),
+		"startVerifyUpstream": Apply(To("upstreams.init.strategy", &cfg.Upstreams.Init), func(value bool) {
+			if value {
+				cfg.Upstreams.Init.Strategy = InitStrategyFailOnError
+			} else {
+				cfg.Upstreams.Init.Strategy = InitStrategyFast
+			}
+		}),
+	})
+
+	usesDepredOpts = cfg.Blocking.migrate(logger) || usesDepredOpts
+	usesDepredOpts = cfg.HostsFile.migrate(logger) || usesDepredOpts
+
+	return usesDepredOpts
+}
+
+func (cfg *Config) validate(logger *logrus.Entry) {
+	cfg.MinTLSServeVer.validate(logger)
+	cfg.Upstreams.validate(logger)
+}
+
+func ConvertPort(in string) (uint16, error) {
+	const (
+		base    = 10
+		bitSize = 16
+	)
+
+	p, err := strconv.ParseUint(strings.TrimSpace(in), base, bitSize)
+
+	if err != nil {
+		return 0, err
+	}
+	return uint16(p), nil
+}
